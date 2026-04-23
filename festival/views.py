b81f61d327a88c0254f8a5f6e2418ab13ff008cc
RodrigoRamos22405227 <a22405227@alunos.ulht.pt>
2026-04-23 15:45:03 +0000

    commit para update dos templates

--- festival/views.py
+++ festival/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render, redirect
 from .models import Dia, Palco, Concerto
 
 def home_view(request):
-    return render(request, 'festival/home.html')
+    return render(request, 'festival/index.html')
 
 def dias_view(request):
     context = {'dias': Dia.objects.all()}
-    return render(request, 'festival/dias.html', context)
+    return render(request, 'festival/index.html', context)
 
 def dia_view(request, dia_id):
     dia = Dia.objects.get(id=dia_id)
@@ -15,7 +15,7 @@ def dia_view(request, dia_id):
         'dia': dia,
         'concertos': concertos
     }
-    return render(request, 'festival/dia.html', context)
+    return render(request, 'festival/index.html', context)
 
 def palcos_view(request):
     context = {'palcos': Palco.objects.all()}
